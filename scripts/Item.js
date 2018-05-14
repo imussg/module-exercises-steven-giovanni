70d3103f9a35f94be5cb040974cfad7654dca149
Steven Imus <imussg@gmail.com>
2018-05-14 14:34:29 -0400

    problem 1 of the second set

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,10 +1,31 @@
 'use strict';
 
+
 const Item = (function(){
+	
+	const validateName = (function(name) {
+		// console.log(name ? name : '');
+		try{
+			if(name === '') {
+				throw 'Name does not exist.';
+			}
+		}
+		catch(err) {
+			console.log("Cannot add item: " + err.message);
+		}
+	});
 
-  return {};
+	function create(name) {
+		return {
+			id: cuid(),
+			name: name,
+			checked: false
+		};
+	}
 
-
-
+  	return {
+  		validateName: validateName,
+  		create: create
+  	};
 
 }());
--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,16 @@
 // };
 
 $(document).ready(function() {
-  shoppingList.bindEventListeners();
-  shoppingList.render();
+	shoppingList.bindEventListeners();
+	shoppingList.render();
+	const itemNames = [ 'asdf', 'apples', 'pears' ];
+	itemNames.forEach(name => {
+		try {
+		    Item.validateName(name);
+		    store.items.push(Item.create(name));
+	  	} catch(error) {
+	    console.log('Cannot add item: ' + error.message);
+	  }
+	});
+	shoppingList.render();
 });
